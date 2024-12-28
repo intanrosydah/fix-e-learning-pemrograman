dc18c502a07d00ad2f6355169002ba61d2f144bd
Yasir <email_anda@example.com>
2024-12-28 11:41:39 +0700

    updte mnjmn pengguna

--- Admin/monitoring-aktivitas.php
+++ Admin/monitoring-aktivitas.php
@@ -66,7 +66,7 @@ $result = $pdo->query($sql);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SISFO - Progres Kelas</title>
+    <title>SISFO - Monitoring Aktivitas</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
     body {
@@ -117,7 +117,7 @@ $result = $pdo->query($sql);
   <div class="content pt-5 mt-3">
     <div class="container mt-5">
       <div class="d-flex justify-content-between align-items-center mb-4">
-        <h3>PROGRES KELAS</h3>
+        <h3>MONITORING AKTIVITAS</h3>
         <div>
           <button class="btn btn-primary me-2" data-bs-toggle="modal" data-bs-target="#addModal">Tambah Progres</button>
           <button class="btn btn-primary me-2">Excel</button>
